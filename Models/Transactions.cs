63809cfeea7671ff995993769fd52739eff095ce
Chris Lenz <chris_lnz@yahoo.com>
2026-08-30 10:58:27 -0400

    Some analysis added

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace CreditCardAnalyzer.Models
 {
@@ -16,4 +18,29 @@ namespace CreditCardAnalyzer.Models
         // Calculate net amount (Debit - Credit)
         public decimal NetAmount => Debit - Credit;
     }
+
+    public class TransactionGroup : List<Transaction>
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal TotalSpent => this.Sum(t => t.Debit);
+        public decimal TotalCredits => this.Sum(t => t.Credit);
+    }
+
+    public class AnalysisSummary
+    {
+        public Dictionary<string, TransactionGroup> MonthlyTotals { get; } = new();
+        public Dictionary<string, TransactionGroup> ByMonth { get; } = new();
+        public Dictionary<string, TransactionGroup> ByCategory { get; } = new();
+        public Dictionary<string, TransactionGroup> ByMerchant { get; } = new();
+
+        public decimal TotalSpent => MonthlyTotals.Values.Sum(group => group.TotalSpent);
+    }
+
+    public class AnalysisViewModel
+    {
+        public List<Transaction> Transactions { get; set; } = new();
+        public AnalysisSummary Summary { get; set; } = new();
+    }
 }
+
